bd84dddceeed3bdd7ecec2eeb8784ccaff53e4df
raj879720@gmail.com <Theflash@1>
2024-02-04 08:47:51 +0530

    Initial ef setup

---
+++ Entityapp/DataModel/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Entityapp.DataModel
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+    }
+}
--- Entityapp/Program.cs
+++ Entityapp/Program.cs
@@ -1,5 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+using Entityapp.DataModel;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("appDb")));
 // Add services to the container.
 
 builder.Services.AddControllers();
